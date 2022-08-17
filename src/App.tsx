c390cf0de4f612389df8d46a18e702ba6aa41d33
BogyeongKim <boggyhint@gmail.com>
2022-08-17 15:22:01 +0900

    fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Col, Container, Form, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import './App.css';
 
 import { MemberForm } from './components/memberform';
