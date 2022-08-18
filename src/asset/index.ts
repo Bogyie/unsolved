79c2e95ed0d3f252ed5b77a04aad775031b7d528
BogyeongKim <boggyhint@gmail.com>
2022-08-18 22:41:47 +0900

    commit

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -1,4 +1,11 @@
-import { B, B1, B2, B3, B4, B5, D, D1, D2, D3, D4, D5, G, G1, G2, G3, G4, G5, P, P1, P2, P3, P4, P5, R, R1, R2, R3, R4, R5, S, S1, S2, S3, S4, S5 } from "./tier"
+import {
+    B, B1, B2, B3, B4, B5,
+    S, S1, S2, S3, S4, S5, 
+    G, G1, G2, G3, G4, G5, 
+    P, P1, P2, P3, P4, P5, 
+    D, D1, D2, D3, D4, D5, 
+    R, R1, R2, R3, R4, R5,
+} from "./tier"
 
 export const TierIcon = {
     'b': B(),
